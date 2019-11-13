e7971217d22adeaa9baadb8f30cedefcf601ad28
Bertrand Marron <bertrand.marron@gmail.com>
2019-11-13 19:11:04 +0100

    Add Sellers.getServiceStatus API method

---
+++ __tests__/lib/client/parsers/sellers/get-service-status.js
@@ -0,0 +1,23 @@
+const parseXml = require('../../../../../lib/client/parsers')
+const parseGetServiceStatusResponse = require('../../../../../lib/client/parsers/base/get-service-status-response')
+
+describe('lib.client.parsers.base.get-service-status-response', () => {
+  it('should parse the GetServiceStatusResponse example response from MWS doc', () => {
+    const doc = parseXml(
+      `<?xml version="1.0"?>
+      <GetServiceStatusResponse xmlns="https://mws.amazonservices.com/Sellers/2011-07-01">
+        <GetServiceStatusResult>
+          <Status>GREEN</Status>
+          <Timestamp>2010-09-24T21:38:09.676Z</Timestamp>
+        </GetServiceStatusResult>
+      </GetServiceStatusResponse>`
+    )
+
+    const res = parseGetServiceStatusResponse(
+      '/sellers:GetServiceStatusResponse',
+      doc
+    )
+
+    expect(res).toMatchSnapshot()
+  })
+})
