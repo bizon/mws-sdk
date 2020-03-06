3bc7f666ae6995f8604194d0e9e41b15d8a31227
Bertrand Marron <bertrand.marron@gmail.com>
2020-03-06 11:11:41 +0100

    Update Orders model to support new properties
    Also fix some copy/paste issues and remove some old properties.

--- __tests__/lib/client/models/orders.js
+++ __tests__/lib/client/models/orders.js
@@ -61,6 +61,7 @@ describe('lib.client.models.orders', () => {
                 <IsBusinessOrder>false</IsBusinessOrder>
                 <IsPrime>false</IsPrime>
                 <IsPremiumOrder>false</IsPremiumOrder>
+                <IsGlobalExpressEnabled>false</IsGlobalExpressEnabled>
               </Order>
             </Orders>
           </GetOrderResult>
@@ -142,6 +143,7 @@ describe('lib.client.models.orders', () => {
                 <PurchaseOrderNumber>PO12345678</PurchaseOrderNumber>
                 <IsPrime>false</IsPrime>
                 <IsPremiumOrder>false</IsPremiumOrder>
+                <IsGlobalExpressEnabled>false</IsGlobalExpressEnabled>
               </Order>
               <Order>
                 <AmazonOrderId>483-3488972-0896720</AmazonOrderId>
@@ -185,6 +187,7 @@ describe('lib.client.models.orders', () => {
                 <IsBusinessOrder>false</IsBusinessOrder>
                 <IsPrime>false</IsPrime>
                 <IsPremiumOrder>false</IsPremiumOrder>
+                <IsGlobalExpressEnabled>false</IsGlobalExpressEnabled>
               </Order>
               <Order>
                 <AmazonOrderId>058-1233752-8214740</AmazonOrderId>
@@ -241,6 +244,7 @@ describe('lib.client.models.orders', () => {
                 <IsBusinessOrder>false</IsBusinessOrder>
                 <IsPrime>false</IsPrime>
                 <IsPremiumOrder>false</IsPremiumOrder>
+                <IsGlobalExpressEnabled>false</IsGlobalExpressEnabled>
                 <PromiseResponseDueDate>2017-08-31T23:58:44Z</PromiseResponseDueDate>
                 <IsEstimatedShipDateSet>true</IsEstimatedShipDateSet>
               </Order>
@@ -320,6 +324,7 @@ describe('lib.client.models.orders', () => {
                 <PurchaseOrderNumber>PO12345678</PurchaseOrderNumber>
                 <IsPrime>false</IsPrime>
                 <IsPremiumOrder>false</IsPremiumOrder>
+                <IsGlobalExpressEnabled>false</IsGlobalExpressEnabled>
               </Order>
               <Order>
                 <AmazonOrderId>483-3488972-0896720</AmazonOrderId>
@@ -363,6 +368,7 @@ describe('lib.client.models.orders', () => {
                 <IsBusinessOrder>false</IsBusinessOrder>
                 <IsPrime>false</IsPrime>
                 <IsPremiumOrder>false</IsPremiumOrder>
+                <IsGlobalExpressEnabled>false</IsGlobalExpressEnabled>
               </Order>
               <Order>
                 <AmazonOrderId>058-1233752-8214740</AmazonOrderId>
@@ -419,6 +425,7 @@ describe('lib.client.models.orders', () => {
                 <IsBusinessOrder>false</IsBusinessOrder>
                 <IsPrime>false</IsPrime>
                 <IsPremiumOrder>false</IsPremiumOrder>
+                <IsGlobalExpressEnabled>false</IsGlobalExpressEnabled>
                 <PromiseResponseDueDate>2017-08-31T23:58:44Z</PromiseResponseDueDate>
                 <IsEstimatedShipDateSet>true</IsEstimatedShipDateSet>
               </Order>
