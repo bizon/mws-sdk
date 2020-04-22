3f852b2793508ff67459f5fd195639fea2b50fe4
Bertrand Marron <bertrand.marron@gmail.com>
2020-04-22 15:52:53 +0200

    Rewrite error tests using nock

--- __tests__/lib/client/error.js
+++ __tests__/lib/client/error.js
@@ -1,4 +1,5 @@
-const {HTTPError} = require('got')
+const nock = require('nock')
+const got = require('got')
 
 const MWSError = require('../../../lib/client/error')
 
@@ -20,8 +21,8 @@ describe('lib.client.error', () => {
 
   it('should fail if resource or action is not defined', () => {
     const tests = [
-      () => new MWSError(new HTTPError({})),
-      () => new MWSError(new HTTPError({}), 'Order')
+      () => new MWSError(new got.HTTPError({})),
+      () => new MWSError(new got.HTTPError({}), 'Order')
     ]
 
     for (const test of tests) {
@@ -29,17 +30,23 @@ describe('lib.client.error', () => {
     }
   })
 
-  it('should not allow updating error properties', () => {
+  it('should not allow updating error properties', async () => {
+    nock('http://example.org').get('/').reply(
+      400,
+      `<ErrorResponse>
+        <Error>
+          <Message>This is a message</Message>
+        </Error>
+      </ErrorResponse>`
+    )
+
+    const response = await got.get('http://example.org', {
+      retry: 0,
+      throwHttpErrors: false
+    })
+
     const error = new MWSError(
-      new HTTPError({
-        statusCode: 400,
-        statusMessage: 'Something wrong happened',
-        body: `<ErrorResponse>
-          <Error>
-            <Message>This is a message</Message>
-          </Error>
-        </ErrorResponse>`
-      }),
+      new got.HTTPError(response),
       'ResourceName',
       'Function'
     )
@@ -60,40 +67,52 @@ describe('lib.client.error', () => {
     }
   })
 
-  it('should create a MWSError and parse the response body', () => {
+  it('should create a MWSError and parse the response body', async () => {
+    nock('http://example.org').get('/').reply(
+      400,
+      `<ErrorResponse>
+        <Error>
+          <Type>Foo</Type>
+          <Code>Bar</Code>
+          <Message>This is a message</Message>
+        </Error>
+        <ResponseMetadata>
+          <RequestId>some_request_id</RequestId>
+        </ResponseMetadata>
+      </ErrorResponse>`
+    )
+
+    const response = await got.get('http://example.org', {
+      retry: 0,
+      throwHttpErrors: false
+    })
+
     const error = new MWSError(
-      new HTTPError({
-        statusCode: 400,
-        statusMessage: 'Something wrong happened',
-        body: `<ErrorResponse>
-          <Error>
-            <Type>Foo</Type>
-            <Code>Bar</Code>
-            <Message>This is a message</Message>
-          </Error>
-          <ResponseMetadata>
-            <RequestId>some_request_id</RequestId>
-          </ResponseMetadata>
-        </ErrorResponse>`
-      }),
+      new got.HTTPError(response),
       'ResourceName',
       'Function'
     )
 
-    expect(error.message).toBe('ResourceName.Function error: Response code 400 (Something wrong happened)')
+    expect(error.message).toBe('ResourceName.Function error: Response code 400 (Bad Request)')
     expect(error.body).toMatchSnapshot()
   })
 
-  it('should create a MWSError and parse the response body using a custom error response parser', () => {
+  it('should create a MWSError and parse the response body using a custom error response parser', async () => {
+    nock('http://example.org').get('/').reply(
+      401,
+      `<ErrorResponse xmlns="https://mws.amazonservices.com/Sellers/2011-07-01">
+        <Foo>Hello</Foo>
+        <Bar>World!</Bar>
+      </ErrorResponse>`
+    )
+
+    const response = await got.get('http://example.org', {
+      retry: 0,
+      throwHttpErrors: false
+    })
+
     const error = new MWSError(
-      new HTTPError({
-        statusCode: 400,
-        statusMessage: 'Something wrong happened',
-        body: `<ErrorResponse xmlns="https://mws.amazonservices.com/Sellers/2011-07-01">
-          <Foo>Hello</Foo>
-          <Bar>World!</Bar>
-        </ErrorResponse>`
-      }),
+      new got.HTTPError(response),
       'Sellers',
       'GetSomething',
       (key, node) => ({
@@ -102,8 +121,8 @@ describe('lib.client.error', () => {
       })
     )
 
-    expect(error.message).toBe('Sellers.GetSomething error: Response code 400 (Something wrong happened)')
+    expect(error.message).toBe('Sellers.GetSomething error: Response code 401 (Unauthorized)')
     expect(error.body).toMatchSnapshot()
-    expect(error.response.statusCode).toBe(400)
+    expect(error.response.statusCode).toBe(401)
   })
 })
